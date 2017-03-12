811628f06794e73fadd4dd8f882299f908904c73
tnmypndy <tnmypndy1997@gmail.com>
2017-03-13 00:51:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -90,6 +91,16 @@ app.get('/:articleName',function (req, res)
  //articles[articleName]=={}   content object for article one
  var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
+});
+function hash (input, salt)
+{
+    //how do  we create a hash
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function (req, res){
+var hashedString = hash(req.params.input, 'this-is-some-random-string');
+res.send(hashedString);
 });
 
 app.get('/articletwo',function (req, res)
